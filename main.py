2f5d1d767b4f2b7529504c94103137823e857c99
Hne <hemantgupta0404@gmail.com>
2021-03-15 10:24:31 +0530

    added add funtiion to add new words and also add the edit function to edit the words and done some changes

--- main.py
+++ main.py
@@ -1,53 +1,71 @@
-from flask import Flask , render_template , request , redirect
+from flask import Flask , render_template , request , redirect , flash
 import json
+import os
 
 with open("config.json", 'r') as c:
     params = json.load(c)['params']
 
 app = Flask(__name__)
 
+app.secret_key = 'the-random-string'
+
 @app.route('/')
-def base():       
+def base():        
     return render_template('base.html',params=params)
     
 
 @app.route('/index' , methods=['GET', 'POST'] ) 
 def index(): 
+    with open("config.json") as c:
+        params = json.load(c)['params']
     if request.method == 'POST':
         text = request.form.get('word')
         mean = params[text][0]
         uses = params[text][1]
-        rword = params[text][2]
-        print(text)     
-    return render_template('index.html' , text=text , uses=uses , mean=mean , rword=rword)
+        rword = params[text][2]   
+    return render_template('index.html' , params=params , text=text , uses=uses , mean=mean , rword=rword)
     
 
 @app.route('/add' , methods=['GET', 'POST'] )
 def add():
     if request.method == 'POST':
-        word = request.form.get('word')
+        text = request.form.get('word')
         hword = request.form.get('hword')
         uses = request.form.get('use')
         rword = request.form.get('rword')
         with open("config.json", "r+") as f:
             data = json.load(f)
-            data["params"][word] =[hword,uses,rword]
+            data["params"][text] =[hword,uses,rword]
             data.update(data)
             f.seek(0)
             json.dump(data , f , indent=4 , sort_keys=True)
-        with open("config.json", 'r') as f:
-            data = json.load(f)["params"]
-            data.update(data)
+        flash("Word Added Successfully", "success")
     return render_template('add.html' , params = params )
 
-@app.route('/delete/<string:word>' , methods=['GET' , 'POST'] )
-def delete(word):
+
+@app.route('/edit/<string:text>' , methods=['GET' , 'POST'] )
+def edit(text):
+    filename = os.path.join('config.json')
+    with open(filename) as c:
+        params = json.load(c)['params']
+        mean = params[text][0]
+        usess = params[text][1]
+        rwords = params[text][2]  
     if request.method == 'POST':
-        word =  request.form.get('word')
-        with open('config.json') as data_file:
-            data = json.load(data_file)
-            data1 = json.dumps(data)
-            for word in data1:
-                del data1["params"][word]
-    return redirect('/', word=word )
-app.run(debug=True)
+        text = request.form.get('word')
+        hword = request.form.get('hword')
+        uses = request.form.get('use')
+        rword = request.form.get('rword')
+        with open("config.json", "r+") as f:
+            data = json.load(f)
+            data['params'][text] =[hword,uses,rword]
+            data.update(data)
+            f.seek(0)
+            f.truncate()
+            json.dump(data , f , indent=4 , sort_keys=True)
+        flash("Word Edit Successfully", "success")
+        
+    return render_template('edit.html' , params = params  , text=text , usess=usess , mean=mean , rwords=rwords)
+
+
+app.run( use_reloader=True,debug=True, port=5000 )
